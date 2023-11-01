2b1c6f01d71ae6a3e3b5670a55e654e404ccd08d
dawoud <mhmaddawoud20@gmail.com>
2023-11-01 13:48:26 +0200

    Adding 10-binary_tree_depth.c filw

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
 	left_height = binary_tree_height(tree->left);

f319aa31852f2c84113da6119d8ca583dd61e383
dawoud <mhmaddawoud20@gmail.com>
2023-11-01 13:42:38 +0200

    Adding 9-binary_tree_height.c file

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,7 +3,6 @@
 /**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure
- *
  * Return: The height of the binary tree, or 0 if the tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)

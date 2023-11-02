155e4add11fbb56feb9d39f644e1986e2e4d09f4
dawoud <mhmaddawoud20@gmail.com>
2023-11-02 07:15:56 +0200

    Adding 17-binary_tree_sibling.c file

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 
 	if (node->parent->left == node)
-		return node->parent->right;
+		return (node->parent->right);
 	else
-		return node->parent->left;
+		return (node->parent->left);
 }

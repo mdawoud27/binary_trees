8a5fb5b94b023bd1c9920e66ee100db20246987f
dawoud <mhmaddawoud20@gmail.com>
2023-11-02 06:47:16 +0200

    Adding 16-binary_tree_is_perfect.c file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,6 +9,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t left_height, right_height, left_perfect, right_perfect;
 
+	if (!tree)
+		return (0);
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
